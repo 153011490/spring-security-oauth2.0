ead7c77eb8137495e18c2ec3565fb7869029a392
lrh <153011490@qq.com>
2020-04-21 21:46:21 +0800

    init project

---
+++ order/src/main/java/com/samson/order/OrderApplication.java
@@ -0,0 +1,17 @@
+package com.samson.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
